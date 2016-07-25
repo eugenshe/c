5a0fb5dda1d01b4dd2bb737e1d7cd36dbebbc8f2
Evgeniy Shevchuk <evgenshe@gmail.com>
2016-07-25 15:46:41 +0300

    add empty db

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -13,7 +13,6 @@ class SiteController extends Controller
 	public function actionError()
 	{
 		if($error = Yii::app()->errorHandler->error) {
-			var_dump($error); die;
 			if(Yii::app()->request->isAjaxRequest) {
 			    echo $error['message'];
 			} else {
